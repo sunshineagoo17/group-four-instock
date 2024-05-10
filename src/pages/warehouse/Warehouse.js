fff8ac89b0aac5d8dbbc5b911962b5816bddb8ba
shazmrd <shaz.mrd@gmail.com>
2024-05-10 15:23:14 -0400

    remove src/pages/warehouse

--- src/pages/warehouse/Warehouse.js
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-import './Warehouse.scss';
-
-const Warehouse = () => {
-  const { warehouseId } = useParams();
-
-  return <div>Warehouse id is:{warehouseId}</div>;
-};
-export default Warehouse;
