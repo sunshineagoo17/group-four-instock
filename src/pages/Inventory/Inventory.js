9a21694beb83c46aaca1a32fb6bf222af5a5cda3
shazmrd <shaz.mrd@gmail.com>
2024-05-15 12:47:49 -0400

    Inventory list fetching data from api and functional

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -11,25 +11,41 @@ const Inventory = () => {
 
   //Init InventoryList
   const [inventoryList, setInventoryList] = useState([]);
+  // for Inventory Item Detail
   const [inventoryDetail, setInventoryDetail] = useState([]);
 
-  //
+  //Fetching Data from API
   useEffect(() => {
-    // Fetch the list of Inventory
-    axios
-      .get(`${URL}/inventories`)
-      .then((response) => {
-        const data = response.data;
-        setInventoryList(data);
-      })
-      .catch((error) => console.error('Error fetching inventories:', error));
-  }, []);
+    if (typeof inventoryId === 'undefined') {
+      // Fetch the list of Inventory
+      axios
+        .get(`${URL}/inventories`)
+        .then((response) => {
+          const data = response.data;
+          setInventoryList(data);
+        })
+        .catch((error) => console.error('Error fetching inventories:', error));
+    } else {
+      // Fetch Item Details
+      axios
+        .get(`${URL}/inventories/${inventoryId}`)
+        .then((response) => {
+          const data = response.data;
+          setInventoryDetail(data);
+        })
+        .catch((error) =>
+          console.error('Error fetching inventory Item Details:', error)
+        );
+    }
+  }, [inventoryId]);
 
   return (
     <div className='page-max-width'>
+      {/* Render Inventory List if InvetoryId is undefined*/}
       {typeof inventoryId == 'undefined' && inventoryList && (
         <InventoryList inventoryList={inventoryList} />
       )}
+      {console.log(inventoryDetail)}
     </div>
   );
 };
