37eded6d9954efd3846ec04370ebcb3d30fd2d2d
Camilo <user@CAmilos-MacBook-Pro.local>
2024-05-17 13:14:56 -0700

    desktop screen done

--- src/components/AddInventory/AddInventory.jsx
+++ src/components/AddInventory/AddInventory.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './AddInventory.scss';
 import arrowDrop from '../../assets/images/arrow_drop_down-24px.svg';
 import ArrowBack from '../../assets/images/arrow_back-24px.svg';
+import { Link } from 'react-router-dom';
 
 function AddInventory() {
+
   return (
     <div className='inventory-add'>
            <div className='inventory-add-header list-padding-side'>
-            <img src={ArrowBack}></img>
+           <Link to='/inventory'><img src={ArrowBack}></img> </Link> 
               <h1 className='inventory-add-header__title txt-header txt-black'>Add New Inventory Item</h1>
            </div>
         <div className='inventory-add-form__container'>
@@ -44,7 +46,7 @@ function AddInventory() {
                 <input className='inventory-add-input' placeholder='0'></input><br />
                 <label className='inventory-add-label'>Wharehouse</label><br />
                 <div className='inventory-add-icon__container'>
-                <img src={arrowDrop}></img>
+                <img  alt='arrow-drop' src={arrowDrop}/>
                 <input className='inventory-add-input' placeholder='Please select'></input>
                 </div>
           </form>
