80e6f3b789aa9de095ca436be64d238a6f6bf54c
sunshine-agoo <sun.molina@gmail.com>
2024-05-19 13:13:47 -0400

    Currently testing feature NPT4-24 with updated backend code

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <div className='page-max-width'>{children}</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
