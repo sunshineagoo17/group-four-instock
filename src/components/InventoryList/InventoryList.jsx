e2d56997bfaec20583a035d2a0fb161b4792f2f6
sunshine-agoo <sun.molina@gmail.com>
2024-05-20 01:16:12 -0400

    Cleaned up code and resolved merge conflicts

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,8 +1,8 @@
 import InventoryListRow from '../InventoryListRow/InventoryListRow';
 import { useState, useEffect } from 'react';
 import './InventoryList.scss';
-
 import sortIcon from '../../assets/images/sort-24px.svg';
+import { Link } from 'react-router-dom';
 
 const InventoryList = ({ fetchFn }) => {
   const [inventoryList, setInventoryList] = useState([]);
@@ -10,9 +10,8 @@ const InventoryList = ({ fetchFn }) => {
   // Fetching Data from API
   useEffect(() => {
     // Fetch the list for invetory
-        fetchFn('/inventories').then(res=> setInventoryList(res));
-    }, [fetchFn]);
-
+    fetchFn('/inventories').then((res) => setInventoryList(res));
+  }, [fetchFn]);
   return (
     <div className='inventory-list'>
       <div className='inventory-list-header list-padding-side'>
@@ -23,9 +22,13 @@ const InventoryList = ({ fetchFn }) => {
           className='inventory-list-header__search input txt-m txt-black'
           placeholder='Search...'
         />
-        <button className='inventory-list-header__add-btn btn txt-section'>
-          + Add New Item
-        </button>
+        <Link
+          className='inventory-list-header__add-btn btn txt-section'
+          to={'add-inventory'}>
+          <button className='inventory-list-header__add-btn btn txt-section'>
+            + Add New Item
+          </button>{' '}
+        </Link>
       </div>
       <div className='divider hide-tablet'></div>
       <div className='inventory-list__filter list-padding-side'>
