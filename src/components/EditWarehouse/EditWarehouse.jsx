794ed48f0c490e33d5f30d22cd14295d2c785b70
sunshine-agoo <sun.molina@gmail.com>
2024-05-21 14:47:38 -0400

    Fixed ESLint error

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -18,7 +18,7 @@ const EditWarehouse = () => {
    
     //Regex functions for validation 
     const phoneRegex = /^\+?(\d{1,4})?[\s-]?(\(?\d{3}\)?)[\s-]?(\d{3})[\s-]?(\d{4})$/;
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
     const emptyRegex = /^$/;
 
   
