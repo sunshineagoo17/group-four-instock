11ffda87f05219160122c1ef8a0232106e2f1055
sunshine-agoo <sun.molina@gmail.com>
2024-05-22 20:47:28 -0400

    Styled the inventory delte modal to match the mockup

--- src/components/InventoryDeleteModal/InventoryDeleteModal.jsx
+++ src/components/InventoryDeleteModal/InventoryDeleteModal.jsx
@@ -23,7 +23,7 @@ const InventoryDeleteModal = ({ show, onClose, onDelete, itemName }) => {
         </div>
         <div className='inventory-modal__content'>
           <h1 id='modal-title' className='inventory-modal__title'>
-            Delete {itemName} item?
+            Delete {itemName} inventory item?
           </h1>
           <p id='modal-description' className='inventory-modal__txt'>
             Please confirm that you’d like to delete {itemName} from the inventory list. You won’t be able to undo this action.            
