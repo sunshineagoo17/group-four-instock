eff0ed734c9ca3d3adb75f6701c495b9cf890d30
sunshine-agoo <sun.molina@gmail.com>
2024-05-28 19:43:39 -0400

    Changed hover effect for delete and edit icons on all three list pages

--- src/components/WarehouseInventoryListRow/WarehouseInventoryListRow.jsx
+++ src/components/WarehouseInventoryListRow/WarehouseInventoryListRow.jsx
@@ -3,8 +3,6 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import InventoryDeleteModal from '../InventoryDeleteModal/InventoryDeleteModal';
 import './WarehouseInventoryListRow.scss';
-import deleteIcon from '../../assets/images/delete_outline-24px.svg';
-import editIcon from '../../assets/images/edit-24px.svg';
 import rightIcon from '../../assets/images/chevron_right-24px.svg';
 
 const WarehouseInventoryListRow = ({
@@ -43,15 +41,15 @@ const WarehouseInventoryListRow = ({
   return (
     <>
       {/* Adds a divider except for the last item */}
-      <div className='warehouseInventory list-padding-side'>
-        <div className='warehouseInventory__cell'>
-          <div className='warehouseInventory__cell_header txt-slate txt-table txt-bold'>
+      <div className='warehouse-inventory list-padding-side'>
+        <div className='warehouse-inventory__cell'>
+          <div className='warehouse-inventory__cell_header txt-slate txt-table txt-bold'>
             INVENTORY ITEM
           </div>
-          <div className='warehouseInventory__cell_desc warehouseInventory__cell_desc--title txt-m txt-bold txt-indigo'>
+          <div className='warehouse-inventory__cell_desc warehouseInventory__cell_desc--title txt-m txt-bold txt-indigo'>
             <Link
               to={`/inventory/${inventory.id}`}
-              className='txt-indigo warehouseInventory__name'>
+              className='txt-indigo warehouse-inventory__name'>
               {inventory.item_name}
               <img
                 className='right-arrow'
@@ -61,13 +59,13 @@ const WarehouseInventoryListRow = ({
             </Link>
           </div>
         </div>
-        <div className='warehouseInventory__cell'>
-          <div className='warehouseInventory__cell_header txt-slate txt-table txt-bold'>
+        <div className='warehouse-inventory__cell'>
+          <div className='warehouse-inventory__cell_header txt-slate txt-table txt-bold'>
             STATUS
           </div>
-          <div className='warehouseInventory__cell_desc'>
+          <div className='warehouse-inventory__cell_desc'>
             <button
-              className={`txt-table txt-bold warehouseInventory__cell_desc--btn ${
+              className={`txt-table txt-bold warehouse-inventory__cell_desc--btn ${
                 inventory.status.toLowerCase() === 'in stock'
                   ? 'instock'
                   : 'outstock'
@@ -76,40 +74,35 @@ const WarehouseInventoryListRow = ({
             </button>
           </div>
         </div>
-        <div className='warehouseInventory__cell'>
-          <div className='warehouseInventory__cell_header txt-slate txt-table txt-bold'>
+        <div className='warehouse-inventory__cell'>
+          <div className='warehouse-inventory__cell_header txt-slate txt-table txt-bold'>
             CATEGORY
           </div>
-          <div className='warehouseInventory__cell_desc warehouseInventory__cell_desc--generic txt-m txt-black txt-regular'>
+          <div className='warehouse-inventory__cell_desc warehouse-inventory__cell_desc--generic txt-m txt-black txt-regular'>
             {inventory.category}
           </div>
         </div>
-        <div className='warehouseInventory__cell'>
-          <div className='warehouseInventory__cell_header txt-slate txt-table txt-bold'>
+        <div className='warehouse-inventory__cell'>
+          <div className='warehouse-inventory__cell_header txt-slate txt-table txt-bold'>
             QTY
           </div>
-          <div className='warehouseInventory__cell_desc warehouseInventory__cell_desc--generic txt-m txt-black txt-regular'>
+          <div className='warehouse-inventory__cell_desc warehouse-inventory__cell_desc--generic txt-m txt-black txt-regular'>
             {inventory.quantity}
           </div>
         </div>
-        <div className='warehouseInventory__cell warehouseInventory__cell--full-width warehouseInventory__cell--flex-space-between'>
-          <div className='btn-animate'>
-            <img
-              className='warehouseInventory__cell_btn'
-              src={deleteIcon}
-              alt='delete button'
-              onClick={() => handleDeleteClick(inventory)}
-            />
+        <div className='warehouse-inventory__cell warehouse-inventory__cell--full-width warehouse-inventory__cell--flex-space-between'>
+          <div className='btn-animate' onClick={() => handleDeleteClick(inventory)}>
+            <svg className='warehouse-inventory__cell_btn delete-icon' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="#C94515"/>
+            </svg>
           </div>
           <Link
             to={`/inventory/edit-inventory/${inventory.id}`}
             className='edit-btn'>
             <div className='btn-animate'>
-              <img
-                className='warehouseInventory__cell_btn'
-                src={editIcon}
-                alt='edit button'
-              />
+              <svg className='warehouse-inventory__cell_bt edit-icon' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#2E66E6"/>
+              </svg>
             </div>
           </Link>
         </div>
--- src/components/WarehouseListRow/WarehouseListRow.jsx
+++ src/components/WarehouseListRow/WarehouseListRow.jsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
 import './WarehouseListRow.scss';
-import deleteIcon from '../../assets/images/delete_outline-24px.svg';
-import editIcon from '../../assets/images/edit-24px.svg';
 import rightIcon from '../../assets/images/chevron_right-24px.svg';
 
 const WarehouseListRow = ({ warehouse, index, onDeleteClick }) => {
@@ -52,21 +50,16 @@ const WarehouseListRow = ({ warehouse, index, onDeleteClick }) => {
           </div>
         </div>
         <div className='warehouse__cell warehouse__cell--full-width'>
-          <div className='btn-animate'>
-            <img
-              className='warehouse__cell_btn'
-              src={deleteIcon}
-              alt='delete button'
-              onClick={() => onDeleteClick(warehouse)}
-            />
+          <div className='btn-animate' onClick={() => onDeleteClick(warehouse)}>
+            <svg className='warehouse__cell_btn delete-icon' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="#C94515"/>
+            </svg>
           </div>
           <Link to={`/warehouse/edit-warehouse/${warehouse.id}`}>
             <div className='btn-animate'>
-              <img
-                className='warehouse__cell_btn'
-                src={editIcon}
-                alt='edit button'
-              />
+              <svg className='warehouse__cell_btn edit-icon' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#2E66E6"/>
+              </svg>
             </div>
           </Link>
         </div>
